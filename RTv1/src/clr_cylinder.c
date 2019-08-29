fc446d106f0a5a82c7764242e9b3bbb882095b90
Umana Draugr deathlord <udraugr-@oa-f3.21-school.ru>
2019-08-29 20:32:08 +0300

    цилиндр на подходе

--- RTv1/src/clr_cylinder.c
+++ RTv1/src/clr_cylinder.c
@@ -6,7 +6,7 @@
 /*   By: udraugr- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/29 14:14:46 by udraugr-          #+#    #+#             */
-/*   Updated: 2019/08/29 18:13:10 by udraugr-         ###   ########.fr       */
+/*   Updated: 2019/08/29 20:31:45 by udraugr-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,39 @@
  * b = 2 * ( < D ; O - C > - < V ; O - C > * < D ; V > )
  * c = < O - C ; O - C > - < O - C ; V > * < O - C ; V > - R * R
  * where :
- * D - vectot of view
+ * D - vector of view
  * V - vector of cylinder's axis (len(V) == 1)
  * O - point of view
  * R - radius of cylinder
  * C - point on axis of cylinder
 */
+coefficient_quadratic_equation[0]
 
-void				clr_cylinder(t_obj *cam, t_obj *plane, t_ray *ray)
+void				clr_cylinder(t_obj *cam, t_obj *current_obj, t_ray *ray)
 {
+	t_cylinder		*cylinder;
+	float			coefficient_quadratic_equation[3];
+	float			roots[2];
+	float			o_c[3];
+	float			tmp;
 
+	cylinder = current_obj;
+	subtraction_point(cam->shapes, cylinder->cylinder_strart, o_c);
+
+	tmp = dot_product(ray->d, cylinder->cylinder_axis);
+	coefficient_quadratic_equation[0] = dot_product(ray->d, ray->d) - tmp * tmp;
+	
+	tmp = dot_product(o_c, cylinder->cylinder_axis) *
+						dot_product(ray->d, cylinder->cylinder_axis);
+	coefficient_quadratic_equation[1] = 2 * (dot_product(ray->d, o_c) - tmp);
+
+	tmp = dot_product(o_c, cylinder->cylinder_axis);
+	coefficient_quadratic_equation[2] =
+		dot_product(o_c, o_c) - (tmp * tmp + r * r);
+	if (roots_quadratic_equation(coefficient_quadratic_equation[0],
+								coefficient_quadratic_equation[1],
+								coefficient_quadratic_equation[2],
+								roots) == FAIL)
+		return ;
+	ft_koroche(ray, roots, current_obj);
 }
--- RTv1/src/operation_with_point.c
+++ RTv1/src/operation_with_point.c
@@ -6,20 +6,21 @@
 /*   By: udraugr- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/29 19:29:41 by udraugr-          #+#    #+#             */
-/*   Updated: 2019/08/29 19:36:45 by udraugr-         ###   ########.fr       */
+/*   Updated: 2019/08/29 20:19:39 by udraugr-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rtv1.h"
 
-float			*multiplication_point(float	first_point, float k)
+void			multiplication_point(float	first_point, float k,
+											float destinaton[3])
 {
-	first_point[0] *= k;
-	first_point[1] *= k;
-	first_point[2] *= k;
+	destination[0] = first_point[0] * k;
+	destination[1] = first_point[1] * k;
+	destination[2] = first_point[2] * k;
 }
 
-float			*addition_point(float first_point[3], float second_point[3],
+void			addition_point(float first_point[3], float second_point[3],
 									float destination[3])
 {
 	destination[0] = first_point[0] + second_point[0];
