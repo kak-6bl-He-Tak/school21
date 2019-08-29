9728b3ef80464a3a1df73d133ca4115458fec4bf
Umana Draugr deathlord <udraugr-@oa-f3.21-school.ru>
2019-08-29 21:11:56 +0300

    начало конуса

--- RTv1/src/clr_cone.c
+++ RTv1/src/clr_cone.c
@@ -6,7 +6,7 @@
 /*   By: udraugr- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/29 20:52:10 by udraugr-          #+#    #+#             */
-/*   Updated: 2019/08/29 21:05:05 by udraugr-         ###   ########.fr       */
+/*   Updated: 2019/08/29 21:11:38 by udraugr-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,24 +23,26 @@ void				clr_cone(t_obj *cam, t_obj *current_obj, t_ray *ray)
 	float			tmp;
 
 	cone - current_obj->shape;
-	subtraction_point((float *)cam->shape, cylinder->cylinder_start, o_c);
+	subtraction_point((float *)cam->shape, cone->cone_start, o_c);
 
-	tmp = dot_product(ray->d, cylinder->cylinder_axis);
-	coefficient_quadratic_equation[0] = dot_product(ray->d, ray->d) - tmp * tmp;
+	tmp = dot_product(ray->d, cone->cone_axis);
+	coefficient_quadratic_equation[0] = dot_product(ray->d, ray->d) -
+					(tmp * tmp) * (cone->cone_angle * cone->cone_angle + 1);
 
-	tmp = dot_product(o_c, cylinder->cylinder_axis) *
-						dot_product(ray->d, cylinder->cylinder_axis);
-	coefficient_quadratic_equation[1] = 2 * (dot_product(ray->d, o_c) - tmp);
+	tmp = dot_product(o_c, cone->cone_axis) *
+						dot_product(ray->d, cone->cone_axis);
+	coefficient_quadratic_equation[1] = 2 * (dot_product(ray->d, o_c) -
+							tmp * (cone->cone_angle * cone->cone_angle + 1));
 
-	tmp = dot_product(o_c, cylinder->cylinder_axis);
+	tmp = dot_product(o_c, cone->cone_axis);
 	coefficient_quadratic_equation[2] =
 		dot_product(o_c, o_c) -
-			(tmp * tmp + cylinder->cylinder_radius * cylinder->cylinder_radius);
+			((tmp * tmp) * (cone->cone_angle * cone->cone_angle + 1));
 	if (roots_quadratic_equation(coefficient_quadratic_equation[0],
 								coefficient_quadratic_equation[1],
 								coefficient_quadratic_equation[2],
 								roots) == FAIL)
 		return ;
-	ft_koroche(ray, roots, current_obj);
+	nearest_object(ray, roots, current_obj);
 }
 }
