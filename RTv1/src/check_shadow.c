d6984547e2cf71018a8bcad00df8ca2b1094e85a
Umana Draugr deathlord <udraugr-@ox-o4.21-school.ru>
2019-09-08 16:37:24 +0300

    I was here\ls

--- RTv1/src/check_shadow.c
+++ RTv1/src/check_shadow.c
@@ -6,68 +6,73 @@
 /*   By: dtreutel <dtreutel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/02 19:10:54 by udraugr-          #+#    #+#             */
-/*   Updated: 2019/09/08 12:13:01 by dtreutel         ###   ########.fr       */
+/*   Updated: 2019/09/08 14:50:34 by udraugr-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rtv1.h"
 
-static void		fill_new_cam_and_new_ray(t_obj *camera, t_ray *ray_p_light,
+static void		fill_new_cam_and_new_ray(t_obj *cam, t_ray *ray_p_light,
 									t_ray *ray, t_light *light)
 {
-	camera->shape = (void *)ray->p;
-	camera->type = ray->obj->type;//!CAMERA;
+	cam->shape = (void *)ray->p;
+	cam->type = ray->obj->type;
 	if (light->type == 1)
 	{
-		//subtraction_point(ray->p, light->center, ray_p_light->d);
 		subtraction_point(light->center, ray->p, ray_p_light->d);
 		ray_p_light->t = len_vector(ray_p_light->d);
-		multiplication_point(ray_p_light->d, 1.0 / ray_p_light->t, ray_p_light->d);
+		multiplication_point(ray_p_light->d,
+						1.0 / ray_p_light->t, ray_p_light->d);
 	}
 	else if (light->type == 2)
 	{
 		multiplication_point(light->vector, -1.0, ray_p_light->d);
-		multiplication_point(ray_p_light->d, 1.0 / len_vector(ray_p_light->d), ray_p_light->d);
-		ray_p_light->t = 0.0;//2147483647.0;
+		multiplication_point(ray_p_light->d,
+						1.0 / len_vector(ray_p_light->d), ray_p_light->d);
 	}
+}
+
+static int		check_roots(t_light *light, float roots[2], t_ray ray_p_light)
+{
+	if (roots[0] > 0.00001 &&
+			((light->type == 1 && roots[0] < ray_p_light.t) ||
+			(light->type == 2)))
+		return (FAIL);
+	if (roots[1] > 0.00001 &&
+			((light->type == 1 && roots[1] < ray_p_light.t) ||
+			(light->type == 2)))
+		return (FAIL);
+	return (SUCCESS);
 }
 
 int				check_shadow(t_rt *rt, t_ray *ray, t_light *light)
 {
-	t_obj		camera;
+	t_obj		cam;
 	t_ray		ray_p_light;
 	t_obj		*current_obj;
 	float		result;
 	float		roots[2];
 
 	current_obj = rt->obj;
-	ft_bzero((void *)&camera, sizeof(t_obj));
+	ft_bzero((void *)&cam, sizeof(t_obj));
 	ft_bzero((void *)&ray_p_light, sizeof(t_ray));
-	fill_new_cam_and_new_ray(&camera, &ray_p_light, ray, light);
+	fill_new_cam_and_new_ray(&cam, &ray_p_light, ray, light);
 	while (current_obj->next)
 	{
 		current_obj = current_obj->next;
 		if (current_obj != ray->obj)
 		{
 			if (current_obj->type == SPHERE)
-				result = clr_sphere(&camera, current_obj, &ray_p_light, roots);
+				result = clr_sphere(&cam, current_obj, &ray_p_light, roots);
 			else if (current_obj->type == PLANE)
-				result = clr_plane(&camera, current_obj, &ray_p_light, roots);
+				result = clr_plane(&cam, current_obj, &ray_p_light, roots);
 			else if (current_obj->type == CYLINDER)
-				result = clr_cylinder(&camera, current_obj, &ray_p_light, roots);
+				result = clr_cylinder(&cam, current_obj, &ray_p_light, roots);
 			else if (current_obj->type == CONE)
-				result = clr_cone(&camera, current_obj, &ray_p_light, roots);
-			if (result == SUCCESS)
-			{
-				if (roots[0] > 0.00001 &&
-						((light->type == 1 && roots[0] < ray_p_light.t) ||
-						 (light->type == 2)))
-					return (FAIL);
-				if (roots[1] > 0.00001 &&
-						((light->type == 1 && roots[1] < ray_p_light.t) ||
-						 (light->type == 2)))
-					return (FAIL);
-			}
+				result = clr_cone(&cam, current_obj, &ray_p_light, roots);
+			if (result == SUCCESS &&
+					check_roots(light, roots, ray_p_light) == FAIL)
+				return (FAIL);
 		}
 	}
 	return (SUCCESS);
--- RTv1/src/diffuse_reflection.c
+++ RTv1/src/diffuse_reflection.c
@@ -6,19 +6,70 @@
 /*   By: dtreutel <dtreutel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/03 15:54:27 by dtreutel          #+#    #+#             */
-/*   Updated: 2019/09/05 21:16:59 by dtreutel         ###   ########.fr       */
+/*   Updated: 2019/09/08 16:36:29 by udraugr-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rtv1.h"
 
+static float	not_mirror_light(t_ray *ray, t_light *light)
+{
+	float		intensive_current_light;
+	float		l[3];
+
+	if (light->type == 1)
+	{
+		subtraction_point(light->center, ray->p, l);
+	}
+	else if (light->type == 2)
+	{
+		multiplication_point(light->vector, -1.0, l);
+	}
+		intensive_current_light = light->intensive * dot_product(ray->normal, l) /
+		(len_vector(ray->normal) * len_vector(l));
+	if (dot_product(ray->normal, l) > 0)
+	{
+		return (intensive_current_light);
+	}
+	return (0.0f);
+}
+
+static float	mirror_light(t_ray *ray, t_light *light)
+{
+	float		l[3];
+	float		r[3];
+	float		d_minus[3];
+	float		r_dot_v;
+	float		cos_l_n;
+
+	if (light->type == 1)
+	{
+		subtraction_point(light->center, ray->p, l);
+	}
+	else if (light->type == 2)
+	{
+		multiplication_point(light->vector, -1.0, l);
+	}
+	multiplication_point(ray->d, -1.0, d_minus);
+	multiplication_point(ray->normal,
+			2.0 * dot_product(ray->normal, l), r);
+	subtraction_point(r, l, r);
+	if ((r_dot_v = dot_product(d_minus, r)) > 0.0)
+	{
+		cos_l_n = pow((r_dot_v / (len_vector(r) * len_vector(ray->d))), 50);
+		if (cos_l_n >= 0.85)
+			ray->clr = 0xFFFFFF;
+		return (light->intensive * cos_l_n);
+				//pow((r_dot_v / (len_vector(r) * len_vector(ray->d))), 50));
+	}
+	return (0.0f);
+}
+
 float			diffuse_reflection(t_rt *rt, t_ray *ray)
 {
 	float		intensive_light;
-	float		intensive_current_light;
 	t_light		*light;
 	t_obj		*curr_light;
-	float		l[3];
 
 	intensive_light = 0.0;
 	curr_light = rt->light;
@@ -29,44 +80,8 @@ float			diffuse_reflection(t_rt *rt, t_ray *ray)
 			intensive_light += light->intensive;
 		else if (light->type != 0 && check_shadow(rt, ray, light) == SUCCESS)
 		{
-			if (light->type == 1)
-			{
-				subtraction_point(light->center, ray->p, l);
-				multiplication_point(l, 1.0 / len_vector(l), l);
-			}
-			else if (light->type == 2)
-			{
-				multiplication_point(light->vector, -1.0, l);
-			}
-			intensive_current_light = light->intensive *
-					dot_product(ray->normal, l) /
-						(len_vector(ray->normal) * len_vector(l));
-			if (dot_product(ray->normal, l) > 0)
-				intensive_light += intensive_current_light;
-			//блики
-	///*
-			//float l[3];
-			float r[3];
-			float d_minus[3];
-			float r_dot_v;
-
-			if (light->type == 1)
-			{
-				subtraction_point(light->center, ray->p, l);
-			}
-			else if (light->type == 2)
-			{
-				multiplication_point(light->vector, -1.0, l);
-			}
-			multiplication_point(ray->d, -1.0, d_minus);
-			multiplication_point(ray->normal,
-								2.0 * dot_product(ray->normal, l), r);
-			subtraction_point(r, l, r);
-			if ((r_dot_v = dot_product(d_minus, r)) > 0.0)
-				intensive_light += light->intensive *
-					pow((r_dot_v / (len_vector(r) * len_vector(ray->d))), 50);
-	//*/
-			//конец бликов
+			intensive_light += not_mirror_light(ray, light);
+			intensive_light += mirror_light(ray, light);
 		}
 		curr_light = curr_light->next;
 	}
