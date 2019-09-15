0ae7f948dc8d14b0300af3031c3b79fe79a361f2
kak-6bl-He-Tak <bychenkoayay@gmail.com>
2019-09-15 09:48:27 +0300

    matrix remaster

--- RTv1/src/rotation.c
+++ RTv1/src/rotation.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   rotation.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: udraugr- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: dtreutel <dtreutel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/09 11:19:51 by udraugr-          #+#    #+#             */
-/*   Updated: 2019/09/09 12:10:21 by udraugr-         ###   ########.fr       */
+/*   Updated: 2019/09/15 09:44:09 by dtreutel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,15 @@ void			rotation_on_z(float vector[3], float angle,
 	int			x;
 	int			y;
 	int			z;
+	float		vector_cpy[3];
 
+	ft_memcpy((void *)vector_cpy, (void *)vector, sizeof(float) * 3);
 	x = 0;
 	y = 1;
 	z = 2;
-	destination[x] = cos(angle) * vector[x] - sin(angle) * vector[y];
-	destination[y] = sin(angle) * vector[x] + cos(angle) * vector[y];
-	destination[z] = vector[z];
+	destination[x] = cos(angle) * vector_cpy[x] - sin(angle) * vector_cpy[y];
+	destination[y] = sin(angle) * vector_cpy[x] + cos(angle) * vector_cpy[y];
+	destination[z] = vector_cpy[z];
 }
 
 void			rotation_on_x(float vector[3], float angle,
@@ -33,13 +35,15 @@ void			rotation_on_x(float vector[3], float angle,
 	int			x;
 	int			y;
 	int			z;
+	float		vector_cpy[3];
 
+	ft_memcpy((void *)vector_cpy, (void *)vector, sizeof(float) * 3);
 	x = 0;
 	y = 1;
 	z = 2;
-	destination[x] = vector[x];
-	destination[y] = cos(angle) * vector[y] - sin(angle) * vector[z];
-	destination[z] = sin(angle) * vector[y] + cos(angle) * vector[z];
+	destination[x] = vector_cpy[x];
+	destination[y] = cos(angle) * vector_cpy[y] - sin(angle) * vector_cpy[z];
+	destination[z] = sin(angle) * vector_cpy[y] + cos(angle) * vector_cpy[z];
 }
 
 void			rotation_on_y(float vector[3], float angle,
@@ -48,11 +52,13 @@ void			rotation_on_y(float vector[3], float angle,
 	int			x;
 	int			y;
 	int			z;
+	float		vector_cpy[3];
 
+	ft_memcpy((void *)vector_cpy, (void *)vector, sizeof(float) * 3);
 	x = 0;
 	y = 1;
 	z = 2;
-	destination[x] = cos(angle) * vector[x] + sin(angle) * vector[z];
-	destination[y] = vector[y];
-	destination[z] = -sin(angle) * vector[x] + cos(angle) * vector[z];
+	destination[x] = cos(angle) * vector_cpy[x] + sin(angle) * vector_cpy[z];
+	destination[y] = vector_cpy[y];
+	destination[z] = -sin(angle) * vector_cpy[x] + cos(angle) * vector_cpy[z];
 }
